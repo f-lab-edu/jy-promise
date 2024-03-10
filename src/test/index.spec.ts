4cb89e12e21dcf8c33a306c5f08ddc4f5b0628e9
ag502 <ag502@naver.com>
2024-03-10 15:54:12 +0900

    add: Promise then 테스트 케이스 추가

--- src/test/index.spec.ts
+++ src/test/index.spec.ts
@@ -1,7 +1,42 @@
+/* eslint-disable jest/no-done-callback */
 import { expect, test } from "@jest/globals";
-// import MyPromise from "../index";
+import MyPromise from "../index";
 
-// eslint-disable-next-line jest/no-done-callback
+test("Promise 콜백 후속 처리 메서드 `then` 테스트", (done) => {
+  /**
+   * @Given MyPromise 인스턴스 생성 및 결괏값 저장 변수 선언
+   */
+  const myPromiseResults: unknown[] = [];
+
+  const myPromise1 = new MyPromise((resolve) => {
+    resolve("SUCCESS!");
+  });
+  const myPromise2 = new MyPromise((_resolve, reject) => {
+    reject("ERROR!");
+  });
+
+  /**
+   * @When
+   */
+  myPromise1.then((res) => {
+    myPromiseResults.push(res);
+  });
+  myPromise2.then(
+    () => {},
+    (err) => {
+      myPromiseResults.push(err);
+    },
+  );
+
+  /**
+   * @Then
+   */
+  setTimeout(() => {
+    expect(myPromiseResults).toEqual(["SUCCESS!", "ERROR!"]);
+    done();
+  });
+});
+
 test("Promise 우선 순위 비교", (done) => {
   /**
    * @Given 출력 값을 저장할 배열생성
